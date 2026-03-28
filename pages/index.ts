78e75f4fc3b4a1b2968c95137ba15052bffac357
Timur Samatov <cooool.loki@gmail.com>
2026-03-28 14:33:03 -0500

    add barrel exports and extend TS path aliases

---
+++ pages/index.ts
@@ -0,0 +1,4 @@
+export { HomePage } from "./HomePage";
+export { AccountOverviewPage } from "./AccountOverviewPage";
+export { TransferPage } from "./TransferPage";
+export { ActivityPage } from "./ActivityPage";
